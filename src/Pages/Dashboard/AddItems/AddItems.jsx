ce5a2f7d0d8038d43b512af83bd8a1aaa756f756
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-11-19 01:06:12 +0600

    add item

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
@@ -7,30 +8,81 @@ const AddItems = () => {
     console.log(data);
   };
   return (
-    <div>
+    <div className="mb-10">
       <SectionTitle
         subHeading="what's new?"
         heading="add an item"
       ></SectionTitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} />
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Recipe Name*</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Recipe Name"
+              className="input input-bordered w-full "
+            />
+          </div>
 
-          <select
-            {...register("category")}
-            className="select select-bordered w-full max-w-xs"
-            defaultValue="" // Use defaultValue prop here
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="dessert">Dessert</option>
-            <option value="drink">Drink</option>
-          </select>
-          <input type="submit" />
+          <div className="flex gap-6">
+            {/* category  */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">Category*</span>
+              </label>
+              <select
+                {...register("category", { required: true })}
+                className="select select-bordered w-full "
+                defaultValue="" // Use defaultValue prop here
+              >
+                <option value="" disabled>
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drink">Drink</option>
+              </select>
+            </div>
+
+            {/* price  */}
+            <div className="form-control w-full my-6">
+              <label className="label">
+                <span className="label-text">Price*</span>
+              </label>
+              <input
+                type="number"
+                {...register("price", { required: true })}
+                placeholder="Recipe Name"
+                className="input input-bordered w-full "
+              />
+            </div>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Recipe Details</span>
+            </label>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </div>
+
+          <div className="form-control w-full my-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input file-input-bordered w-full max-w-xs"
+            />
+          </div>
+          <button className="btn btn-warning gap-2">
+            Add Item <FaUtensils></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
